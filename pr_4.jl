b7604a0546ee533753c8824c3e57d432ca5b25cb
Sofffya <nosenkova.sofya@mail.ru>
2023-06-24 00:45:07 +0300

    Update pr_4.jl

---
+++ pr_4.jl
@@ -0,0 +1,25 @@
+#4 обратный ход алгоритма Жордана-Гаусса
+using LinearAlgebra
+
+function  reverse_gauss(A::AbstractMatrix{T}, b::AbstractVector{T}) where T
+    x = similar(b)
+    N = size(A, 1)
+    for k in 0:n-1
+        x[N-k] = (b[N-k] - sum(A[N-k, N-k+1:end])) .* x[N-k+1:end]/ A[N-k, N-k]
+    end
+    return x 
+end
+
+#5 приведение матрицы к ступенчатому ввиду
+function transform_to_steps!(A::AbstractMatrix; epsilon = 1e-7)
+    @inbounds for k ∈ 1:size(A, 1)
+    absval, Δk = findmax(abs, @view(A[k:end,k]))
+    (absval <= epsilon) && throw("Вырожденая матрица")
+    Δk > 1 && swap!(@view(A[k,k:end]), @view(A[k+Δk-1,k:end]))
+    for i ∈ k+1:size(A,1)
+        t = A[i,k]/A[k,k]
+        @. @views A[i,k:end] = A[i,k:end] - t * A[k,k:end]
+    end
+        end
+    return A
+end
