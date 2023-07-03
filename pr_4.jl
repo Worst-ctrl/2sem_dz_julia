7f48b62fc20175b9b78701015ec24a943027d473
Sofffya <nosenkova.sofya@mail.ru>
2023-07-03 15:07:21 +0300

    1111

--- pr_4.jl
+++ pr_4.jl
@@ -1,4 +1,5 @@
-# 1 
+"1. Написать функцию, вычисляющую n-ю частичную сумму ряда Телора
+Маклорена) функции для произвольно заданного значения аргумента x."
 function exp_(x, n)  #
     ans = 1.0  # суммы ряда
     t = 1.0  
@@ -9,10 +10,11 @@ function exp_(x, n)  #
     return ans  
 end
 
+A = [1 2; 3 4]
+print(A[0])
 
-#4 обратный ход алгоритма Жордана-Гаусса
-using LinearAlgebra
-
+"4. Реализовать алгорим, реализующий обратный ход алгоритма Жордана-Гауссаusing LinearAlgebra
+"
 function  reverse_gauss(A::AbstractMatrix{T}, b::AbstractVector{T}) where T
     x = similar(b)
     N = size(A, 1)
@@ -22,30 +24,24 @@ function  reverse_gauss(A::AbstractMatrix{T}, b::AbstractVector{T}) where T
     return x 
 end
 
-#5 приведение матрицы к ступенчатому ввиду
-
+"5. Реализовать алгоритм, осуществляющий приведение матрицы матрицы к
+ступенчатому виду"
 function transform_to_steps!(A::AbstractMatrix; epsilon = 1e-7)
-    @inbounds for k ∈ 1:size(A, 1)
-    absval, Δk = findmax(abs, @view(A[k:end,k]))
-    (absval <= epsilon) && throw("Вырожденая матрица")
-    Δk > 1 && swap!(@view(A[k,k:end]), @view(A[k+Δk-1,k:end]))
-    for i ∈ k+1:size(A,1)
-        t = A[i,k]/A[k,k]
-        @. @views A[i,k:end] = A[i,k:end] - t * A[k,k:end]
+    n = size(A)
+    for i in 1:n
+        return 0 
     end
-        end
-    return A
 end
 
-#6 - решение слау
-
+"6. Реализовать алгоритм, реализующий метод Жордана-Гаусса решение СЛАУ для
+произвольной невырожденной матрицы (достаточно хорошо обусловленной)."
 function solve_sla(A::AbstractMatrix{T}, b::AbstractVector{T}) where T
     Ab = [A b]
     transform_to_steps!(Ab; epsilon = 10sqrt(eps(T)) maximum(abs,A))
     return reverse_gauss(A, b)
 end
 
-#8. Написать функцию, возвращающую ранг произвольной прямоугольной матрицы (реализуется на базе приведения матрицы к ступенчатому виду).
+"8. Написать функцию, возвращающую ранг произвольной прямоугольной матрицы (реализуется на базе приведения матрицы к ступенчатому виду)."
 function rank!(matrix::AbstractMatrix{Float64})
     transform_to_steps!(Matrix)
       epsilon = 1e-8
@@ -55,4 +51,20 @@ function rank!(matrix::AbstractMatrix{Float64})
     end
     return i-1
   end
-  
+
+"9. Написать функцию, возвращающую определитель произвольной квадратной матрицы (реализуется на основе приведения матрицы к ступенчатому виду)."
+
+
+
+"function transform_to_steps!(A::AbstractMatrix; epsilon = 1e-7)
+@inbounds for k ∈ 1:size(A, 1)
+absval, Δk = findmax(abs, @view(A[k:end,k]))
+(absval <= epsilon) && throw()
+Δk > 1 && swap!(@view(A[k,k:end]), @view(A[k+Δk-1,k:end]))
+for i ∈ k+1:size(A,1)
+    t = A[i,k]/A[k,k]
+    @. @views A[i,k:end] = A[i,k:end] - t * A[k,k:end]
+end
+    end
+return A
+end"
